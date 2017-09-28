cdf70fa6fff907f6b33b9921ee66d3110832cda3
Shelby Neil Smith <ssmith@eezy.com>
2017-09-27 21:07:56 -0500

    Implemented update and edit functionality

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @post = FactoryGirl.create(:post)
     end
 
     it 'can be created' do
-      post = Post.create(date: Date.today, rationale: "Anything")
       expect(@post).to be_valid
     end
 
